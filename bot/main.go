0310782d2faa8512581d35c6d32533bda61b0142
Christoph Raaflaub <raaflaub@puzzle.ch>
2025-04-21 16:36:10 +0200

    make renovate work with docker

--- bot/main.go
+++ bot/main.go
@@ -10,6 +10,7 @@ package main
 import (
 	"context"
 	"dagger/renovate/internal/dagger"
+	"fmt"
 	"time"
 )
 
@@ -121,7 +122,23 @@ func (m *Renovate) GitHub(
 			},
 		).
 		*/
-		// Terminal().
+
+		WithExec(
+			[]string{"bash", "-c", "curl -fsSL https://dl.dagger.io/dagger/install.sh | BIN_DIR=/tmp/ sh"},
+		).
+		Terminal(
+			dagger.ContainerTerminalOpts{
+				ExperimentalPrivilegedNesting: true,
+				InsecureRootCapabilities: true,
+			},
+		).
+		WithUser("root").
+		Terminal(
+			dagger.ContainerTerminalOpts{
+				ExperimentalPrivilegedNesting: true,
+				InsecureRootCapabilities: true,
+			},
+		).
 		WithExec(
 			[]string{"renovate"},
 			dagger.ContainerWithExecOpts{
@@ -131,6 +148,42 @@ func (m *Renovate) GitHub(
 		).
 		Stdout(ctx)
 }
+
+func (m *Renovate) GitHubContainer(
+	ctx context.Context,
+	// config file
+	config *dagger.File,
+	// GitHub token any-personal-user-token-for-github-com-for-fetching-changelogs
+	githubReadToken *dagger.Secret,
+	// GitHub token to do changes
+	githubWriteToken *dagger.Secret,
+	// renovate repository configuration
+	// +optional
+	renovateRepositories string,
+) *dagger.Container {
+	container := m.renovateContainer(ctx, config, githubReadToken).
+		WithEnvVariable("RENOVATE_AUTODISCOVER", "false").
+		WithEnvVariable("RENOVATE_PLATFORM", "github").
+		WithEnvVariable("RENOVATE_GIT_AUTHOR", "Renovate Bot <noreply@github.com>").
+		WithSecretVariable("RENOVATE_TOKEN", githubWriteToken)
+
+	if renovateRepositories != "" {
+		container = container.WithEnvVariable("RENOVATE_REPOSITORIES", renovateRepositories)
+	}
+
+	return container.
+		WithExec(
+			[]string{"bash", "-c", "curl -fsSL https://dl.dagger.io/dagger/install.sh | BIN_DIR=/tmp/ sh"},
+		).
+		WithExec(
+			[]string{"renovate"},
+			dagger.ContainerWithExecOpts{
+				ExperimentalPrivilegedNesting: true,
+				InsecureRootCapabilities: true,
+			},
+		)
+}
+
 
 func (m *Renovate) Hack(
 	ctx context.Context,
@@ -163,14 +216,24 @@ func (m *Renovate) renovateContainer(
 	dockerd := dag.Docker().Engine(dagger.DockerEngineOpts{
 		Persist: false,
 	})
+	endpoint, err := dockerd.Endpoint(ctx, dagger.ServiceEndpointOpts{Scheme: "tcp"})
+	if err != nil {
+		return nil
+	}
+
+	//dockerd := dag.DaggerDev().Service("dockerd")
 
 	container := dag.Container().
 		// official renovate image
 		From("docker.io/renovate/renovate:39.173").
 		//From("registry.access.redhat.com/ubi9/nodejs-20:9.5-1739783265").
 
+
+		
 		WithServiceBinding("dockerd", dockerd).
 		WithEnvVariable("DOCKER_HOST", "tcp://dockerd:2375").
+		WithEnvVariable("_EXPERIMENTAL_DAGGER_RUNNER_HOST", endpoint).
+		
 
 		//WithEnvVariable("GOPROXY", "direct").
 		//WithEnvVariable("GOPROXY", "https://goproxy.io").
@@ -207,7 +270,7 @@ func (m *Renovate) renovateContainer(
 		WithUser("ubuntu").
 
 		//WithUser("1001").
-		WithWorkdir("/usr/src/app/").
+		WithWorkdir("/usr/src/app").
 		WithFile("/tmp/config.json5", config).
 		WithEnvVariable("RENOVATE_CONFIG_FILE", "/tmp/config.json5").
 		/*WithUser("root").
@@ -239,7 +302,9 @@ func (m *Renovate) renovateContainer(
 		WithUser("ubuntu").
 
 
-		WithEnvVariable("LOG_LEVEL", "debug")
+		WithEnvVariable("LOG_LEVEL", "debug").
+		WithEnvVariable("GOPROXY", "direct").
+		WithoutEnvVariable("DOCKER_DEFAULT_PLATFORM") // no solver
 
 	if (token != nil) {
 		 // optional, not used when running on github.com
@@ -248,6 +313,128 @@ func (m *Renovate) renovateContainer(
 
 	// invalidate the cache to never cache the renovate execution
 	container = container.WithEnvVariable("CACHEBUSTER", time.Now().String())
+
+	return container
+}
+
+func (m *Renovate) DnsTest(
+	ctx context.Context,
+	// Docker Engine version
+	// +optional
+	// +default="24.0"
+	version string,
+) (string, error) {
+	return m.DnsTestContainer(ctx, version).Stdout(ctx)
+}
+
+func (m *Renovate) DnsTestContainer(
+	ctx context.Context,
+	// Docker Engine version
+	// +optional
+	// +default="24.0"
+	version string,
+) *dagger.Container {
+	// Start an ephemeral dockerd
+	dockerd := dag.Docker().Engine(dagger.DockerEngineOpts{
+		Persist: false,
+	})
+	endpoint, err := dockerd.Endpoint(ctx, dagger.ServiceEndpointOpts{Scheme: "tcp"})
+	if err != nil {
+		return nil
+	}
+
+	container := dag.Container().
+		From(fmt.Sprintf("index.docker.io/docker:%s-dind", version)).
+
+		WithServiceBinding("dockerd", dockerd).
+		WithEnvVariable("DOCKER_HOST", "tcp://dockerd:2375").
+		WithEnvVariable("_EXPERIMENTAL_DAGGER_RUNNER_HOST", endpoint).
+		// invalidate the cache to never cache the renovate execution
+		WithEnvVariable("CACHEBUSTER", time.Now().String()).
+		WithExec(
+			[]string{"sh", "-c", "apk --no-cache add curl"},
+		).
+		WithExec(
+			[]string{"sh", "-c", "curl -fsSL https://dl.dagger.io/dagger/install.sh | BIN_DIR=/tmp/ sh"},
+		).
+		Terminal(
+			dagger.ContainerTerminalOpts{
+				ExperimentalPrivilegedNesting: true,
+				InsecureRootCapabilities: true,
+			},
+		).
+		WithExec([]string{"whoami"}).
+		WithExec(
+			[]string{"sh", "-c", "/tmp/dagger -m github.com/felipepimentel/daggerverse/essentials/curl@v0.0.0 call get --url proxy.golang.org stdout"},
+			dagger.ContainerWithExecOpts{
+				ExperimentalPrivilegedNesting: true,
+				InsecureRootCapabilities: true,
+			},
+		)
+
+	return container
+}
+
+
+
+/*
+dagger -m bot call dind-container \
+  export --path=./dind-container.tar
+
+docker load -q -i ./dind-container.tar
+docker tag <container-id> dind-container:latest
+
+sudo docker run \
+  -v /var/run/docker.sock:/var/run/docker.sock \
+  -v $(pwd)/config/:/tmp/config/:ro \
+  --entrypoint /bin/sh \
+  -ti \
+  --env LOG_LEVEL="debug" \
+  --env GITHUB_COM_TOKEN=${MY_GH_PAT} \
+  --env RENOVATE_AUTODISCOVER="false" \
+  --env RENOVATE_CONFIG_FILE="/tmp/config/config.json5" \
+  --env RENOVATE_PLATFORM="github" \
+  --env RENOVATE_GIT_AUTHOR="Renovate Bot <noreply@github.com>" \
+  --env RENOVATE_TOKEN=${MY_GH_WRITE_PAT} \
+  --env RENOVATE_REPOSITORIES="['chrira/dagger-module-helm']" \
+  dind-container:latest
+*/
+func (m *Renovate) DindContainer(
+	ctx context.Context,
+	// Docker Engine version
+	// +optional
+	// +default="24.0"
+	version string,
+) *dagger.Container {
+	container := dag.Container().
+
+		// alpine
+		From(fmt.Sprintf("index.docker.io/docker:%s-dind", version)).
+
+		/*
+		WithServiceBinding("dockerd", dockerd).
+		WithEnvVariable("DOCKER_HOST", "tcp://dockerd:2375").
+		WithEnvVariable("_EXPERIMENTAL_DAGGER_RUNNER_HOST", endpoint).
+		*/
+
+		WithWorkdir("/usr/src/app").
+
+		WithExec(
+			[]string{"sh", "-c", "apk --no-cache add curl"},
+		).
+
+		WithExec([]string{"apk","add", "nodejs", "npm"}).
+		//pkg install nodejs
+		WithExec([]string{"npm", "install", "-g", "renovate", "yarn", "pnpm"}) // TODO take fix renovate version and let it update by mr
+
+		// invalidate the cache to never cache the renovate execution
+		//WithEnvVariable("CACHEBUSTER", time.Now().String()).
+
+/*
+		WithExec(
+			[]string{"sh", "-c", "curl -fsSL https://dl.dagger.io/dagger/install.sh | BIN_DIR=/tmp/ sh"},
+		)
+			*/
 
 	return container
 }
